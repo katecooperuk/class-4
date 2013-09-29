6c36714ac90ffe729cbec6391079c7dc6022fe0c
Kate Cooper <kate.cooper@mac.com>
2013-09-29 13:42:17 +0100

    adding comments

--- arrays-demo.php
+++ arrays-demo.php
@@ -22,9 +22,13 @@
 		echo $key." is ".$value."<br>";
 	}
 	?>
+	
 <!----- End of Example of php ----->	
 
+<br>
+
 <!----- Example of above PHP in a table ----->
+
 	<table>
 	<?php
 	foreach($contestants as $key => $value) {
@@ -35,23 +39,35 @@
 	}
 	?>
 	</table>
-<!----- End Example of above PHP in a table ----->
 	
+<!----- End Example of above PHP in a table ----->
+
+<br>	
+
 <!----- Example of PHP using 'endofforeach' ----->
+
 	<?php
 	foreach($contestants as $key => $value):
 		echo $key." is ".$value."<br>";
 	endforeach;
 	?>
-<!----- End Example of PHP using 'endofforeach' ----->
 	
+<!----- End Example of PHP using 'endofforeach' ----->
+
+<br>
+
 <!----- Example of PHP closing each instance of PHP ----->
+
 	<?php foreach($contestants as $key => $value): ?>
 		<?=$key?> is a <?=$value?> <br>
 	<?php endforeach; ?>
+	
 <!----- End Example of PHP closing each instance of PHP ----->
 
+<br>
+
 <!----- Example of above PHP in a table ----->
+
 	<table>
 		<?php foreach($contestants as $key => $value): ?>
 			<tr>
@@ -60,7 +76,38 @@
 			</tr>
 		<?php endforeach; ?>
 	</table>
+	
 <!----- End Example of above PHP in a table ----->
+
+<br>
+
+<!----- Count the Shopping Lists ----->
+
+	<?php
+		echo count($shopping_lists);
+	?>
+
+<!----- End Count the Shopping Lists ----->
+
+<br>
+
+<!----- Count all the rows and columns ----->
+
+	<?php
+		print "There are {$rows} rows and {$cols} columns in the table!";
+	?>
+	
+<!----- End Count all the rows and columns ----->
+
+<br>
+
+<!----- Count all the items on the list ----->
+	
+	<?php	
+		echo (count($shopping_list,COUNT_RECURSIVE)-count($shopping_list,0));
+	?>
+
+<!----- End Count all the items on the list ----->
 
 
 </body>
