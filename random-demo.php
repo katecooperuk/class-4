7042fc8a6b00a4e922a4b65ae583aa1d5ddfb506
Kate Cooper <kate.cooper@mac.com>
2013-09-29 11:53:25 +0100

    renaming random Loop files

---
+++ random-demo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<title>Class 4 - Random</title>
+
+	<?php
+	require('random-logic.php')
+	?>
+	
+	<style>
+		.box {
+			width:<?=$width?>;
+			height:<?=$height?>;
+			background-color: blue;
+			color:grey;
+			float:left;
+			margin:10px;
+}
+	</style>
+
+</head>
+	
+<body>
+
+	<?=$boxes?>
+
+</body>
+</html>
