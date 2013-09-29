60c58ce186b81dfc9e0a83b0360a8b99c2215671
Kate Cooper <kate.cooper@mac.com>
2013-09-29 13:42:28 +0100

    No message

--- arrays-logic.php
+++ arrays-logic.php
@@ -1,8 +1,25 @@
 <?php
 
-$contestants["Sam"] = "Winner";
-$contestants["Bob"] = "Winner";
-$contestants["Jill"] = "Loser";
-$contestants["Eliot"] = "Loser";
+	$contestants["Sam"] = "Winner";
+	$contestants["Bob"] = "Winner";
+	$contestants["Jill"] = "Loser";
+	$contestants["Eliot"] = "Loser";
 
+?>
+
+<?php
+	$shopping_lists['supermarket'] = array('Apples', 'Oranges', 'Milk');
+	$shopping_lists['hardware_store'] = array('Hammer', 'Nails', 'Paint');
+	$shopping_lists['pharmacy'] = array('Bandaids', 'Tylenol');
+?>
+
+<?php
+	$shopping_list = Array(
+	'supermarket' => Array('Apples', 'Oranges', 'Milk'),
+	'hardware_store' => Array('Hammer', 'Nails', 'Paint'),
+	'pharmacy' => Array('Bandaids', 'Tylenol', 'shampoo'),
+	);
+	
+	$rows = count($shopping_list,0);
+	$cols = (count($shopping_list,1)/count($shopping_list,0))-1;
 ?>
