dd0071eac972b4f9553eec5fc9456b1c9070d95b
Kate Cooper <kate.cooper@mac.com>
2013-09-29 10:01:09 +0100

    adding new PHP Loop - Random size boxes

---
+++ loops-demo2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<title>Class 4 - Loops Random</title>
+
+	<?php
+	require('loops-logic2.php')
+	?>
+
+	<link rel="stylesheet" href="loops.css" type="text/css">
+
+</head>
+	
+<body>
+
+	<?=$boxes?>
+
+</body>
+</html>
