8ca9b3441fab18e67ffbf242c97080f160e9f259
Kate Cooper <kate.cooper@mac.com>
2013-09-29 12:42:03 +0100

    adding comments

--- arrays-demo.php
+++ arrays-demo.php
@@ -15,17 +15,52 @@
 	
 <body>
 
+<!----- Example of php ----->
+	
 	<?php
 	foreach($contestants as $key => $value) {
 		echo $key." is ".$value."<br>";
 	}
 	?>
+<!----- End of Example of php ----->	
+
+<!----- Example of above PHP in a table ----->
+	<table>
+	<?php
+	foreach($contestants as $key => $value) {
+		echo "<tr>";
+		echo "<td>".$key."</td>";
+		echo "<td>".$value."</td>";
+		echo "<tr>";
+	}
+	?>
+	</table>
+<!----- End Example of above PHP in a table ----->
 	
+<!----- Example of PHP using 'endofforeach' ----->
 	<?php
 	foreach($contestants as $key => $value):
 		echo $key." is ".$value."<br>";
 	endforeach;
 	?>
+<!----- End Example of PHP using 'endofforeach' ----->
+	
+<!----- Example of PHP closing each instance of PHP ----->
+	<?php foreach($contestants as $key => $value): ?>
+		<?=$key?> is a <?=$value?> <br>
+	<?php endforeach; ?>
+<!----- End Example of PHP closing each instance of PHP ----->
+
+<!----- Example of above PHP in a table ----->
+	<table>
+		<?php foreach($contestants as $key => $value): ?>
+			<tr>
+				<td><?=$key?></td>
+				<td><?=$value?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+<!----- End Example of above PHP in a table ----->
 
 
 </body>
